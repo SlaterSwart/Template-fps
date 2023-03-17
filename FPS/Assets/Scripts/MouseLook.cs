50224bf94111c9eafed75c49381aaaf945b9abcd
SlaterSwart <90404134+SlaterSwart@users.noreply.github.com>
2023-03-17 08:13:26 -0700

    sens

--- FPS/Assets/Scripts/MouseLook.cs
+++ FPS/Assets/Scripts/MouseLook.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class MouseLook : MonoBehaviour
 {   
-    [SerializeField] float sensX = 800000f;
+    [SerializeField] float sensX = 80f;
     [SerializeField] float sensY = 10f;
     float mouseX, mouseY;
 
