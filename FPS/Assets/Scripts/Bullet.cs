ade7100199b3bd07bc9d44282f52be1089e722d6
SlaterSwart <90404134+SlaterSwart@users.noreply.github.com>
2023-05-03 08:58:43 -0700

    Enemy drops Item when killed
    Going to work on a backpack next

--- FPS/Assets/Scripts/Bullet.cs
+++ FPS/Assets/Scripts/Bullet.cs
@@ -6,17 +6,24 @@ public class Bullet : MonoBehaviour
 {
     [SerializeField] float Fov;
     public float life = 3;
+    
+    public GameObject upgradePrefab;
+
+
     private void Awake()
     {
         Destroy(gameObject, life);
     }
     private void OnCollisionEnter(Collision collision)
     {
+        
         if (collision.gameObject.tag == "Enemy"){
             
             collision.gameObject.GetComponentInChildren<Transform>().SetParent(null, true);
-            collision.gameObject.GetComponent<Transform>().position;
+        
+            Instantiate(upgradePrefab, collision.gameObject.GetComponent<Transform>().position, collision.gameObject.GetComponent<Transform>().rotation);
             Destroy(collision.gameObject);
+            
         }
         Destroy(gameObject);
     }
