86967e6a0c3e2997b96346317efc427a2152f286
SlaterSwart <90404134+SlaterSwart@users.noreply.github.com>
2023-05-18 14:21:10 -0700

    Time to start working on Shader!
    fixed the drop

--- FPS/Assets/Scripts/Grenade.cs
+++ FPS/Assets/Scripts/Grenade.cs
@@ -1,18 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
+[RequireComponent(typeof(CharacterController), typeof(PlayerInput))]
 public class Grenade : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    public float throwSpeed;
+
+    private PlayerInput playerInput;
+    private InputAction Throw;
+    public GameObject grenade;
+    public int GrenadeSpeed;
+
+    private void Awake()
     {
-        
+        playerInput = GetComponent<PlayerInput>();
+        Throw = playerInput.actions["Throw"];
+        //Grenade is set in editor
+    }
+    private void Update()
+    {
+        if (Throw.triggered)
+        {
+            grenade.GetComponent<Rigidbody>().velocity = grenade.GetComponent<Transform>().forward * GrenadeSpeed;
+            grenade.GetComponent<Transform>().
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
